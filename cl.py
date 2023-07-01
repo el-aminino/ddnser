c9bfb0299f136230f00fbd3d18b5737eae8e550e
el-aminino <aminmze81@gmail.com>
2023-07-01 11:34:42 +0330

    Little Simple messanger

--- cl.py
+++ cl.py
@@ -1,21 +1,22 @@
 import socket
 
-def clprog():
-    host = "192.168.1.23"
+def clinet():
+    host = "127.0.0.1"
     port = 1381
-    cl_socket = socket.socket()
-    cl_socket.connect((host,port))
-    message = input("   ->  ")
-    while message.lower().strip() != "bye!":
-        cl_socket.send(message.encode())
-        data = cl_socket.recv(1024).decode()
-        print("recieved message is :" + data)
-        message = input("   ->  ")
-        cl_socket.close()
 
+
+    cli_sct = socket.socket()
+    cli_sct.connect((host,port))
     
+    message=input("   ->   ")
 
-
+    while message.lower().strip() != "bye":
+        cli_sct.send(message.encode())
+        data = cli_sct.recv(1024).decode()
+        print("Here is what is recieved: "+ str(data) )
+        message = input("   ->   ")
+    
+    cli_sct.close()
 
 if __name__ == '__main__':
-    clprog()
+    clinet()
--- srv.py
+++ srv.py
@@ -1,14 +1,16 @@
 import socket
 
 def srvprg() :
-    host = "192.168.1.23"
+    host = "127.0.0.1"
     port = 1381
     srv_socket = socket.socket()
     srv_socket.bind((host,port))
     srv_socket.listen(10)
+    
+    conn , address = srv_socket.accept()
+    print("Connection from =" + str(address))
     while True:
-        conn , address = srv_socket.accept()
-        print("Connection from =" + str(address))
+        
     
         data = conn.recv(1024).decode()
         if not data :
@@ -16,9 +18,9 @@ def srvprg() :
         print("connect user" + str(data))
         data = input("  ->  ")
         conn.send(data.encode())
-        conn.close()
+        
     
-    
+    conn.close()
 
 
 
