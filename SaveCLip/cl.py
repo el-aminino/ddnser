1b829702cd373d6b3f33826bfd41fe5aa2045441
el-aminino <aminmze81@gmail.com>
2023-07-01 16:40:01 +0330

    saving ip is completed

--- SaveCLip/cl.py
+++ SaveCLip/cl.py
@@ -7,8 +7,14 @@ port = 1381
 
 #using with statement connection will be closed afte body codes done
 with socket.socket() as client_socket :
+
+    #client connects to specified host and port
     client_socket.connect((host,port))
+
+    #waits for acknowlege from server
     ACK = client_socket.recv(1024).decode()
+
+    #checks for 'took' in acknowlege
     if ACK.lower().strip() == "took":
         print("Server Now Knows me !")
 
