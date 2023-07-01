3deb8917a64a71e4a1655a2ef6ff901b3bd9c56d
el-aminino <aminmze81@gmail.com>
2023-07-01 16:42:26 +0330

    new part of project added

--- SaveCLip/cl.py
+++ SaveCLip/cl.py
@@ -4,18 +4,23 @@ import socket
 #Define server and port
 host = "127.0.0.1"
 port = 1381
-
+ACK = ""
 #using with statement connection will be closed afte body codes done
 with socket.socket() as client_socket :
+    try :
+         #client connects to specified host and port
+        client_socket.connect((host,port))
 
-    #client connects to specified host and port
-    client_socket.connect((host,port))
+        #waits for acknowlege from server
+        ACK = client_socket.recv(1024).decode()
+        
+        
+        #checks for 'took' in acknowlege
+        if ACK.lower().strip() == "took":
+            print("Server Now Knows me !")
+    except :
+        print("server is down or something else is wrong")
 
-    #waits for acknowlege from server
-    ACK = client_socket.recv(1024).decode()
-
-    #checks for 'took' in acknowlege
-    if ACK.lower().strip() == "took":
-        print("Server Now Knows me !")
+        
 
 
--- SaveCLip/srv.py
+++ SaveCLip/srv.py
@@ -8,22 +8,35 @@ ACK = "took"
 fail = "failed"
 
 #Using with statement which closes the connection when done
-with socket.socket() as server_socket :
+while True :
+    with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as server_socket :
 
 
 
+        server_socket.setsockopt(socket.SOL_SOCKET , socket.SO_REUSEADDR,1)
+        server_socket.bind((host,port)) #server is up on specified port
+        server_socket.listen(1024) #waits for any request
 
-    server_socket.bind((host,port)) #server is up on specified port
-    server_socket.listen() #waits for any request
+
+        #creates a connection and also keeps connection datas
+        conn , addr = server_socket.accept()
+
+        client_ip = addr[0] #find client ip address and save it into a variable
+
+        #checks if client's  IP saved or not
+        if client_ip :
+            conn.send(ACK.encode())
+
+            with open("ips" , 'a+') as file:
+                file.write(client_ip)
+                file.write("\n")
 
 
-    #creates a connection and also keeps connection datas
-    conn , addr = server_socket.accept()
 
-    client_ip = addr[0] #find client ip address and save it into a variable
-
-    #checks if client's  IP saved or not
-    if client_ip :
-        conn.send(ACK.encode())
-    else:
-        conn.send(fail.encode())
+        else:
+            conn.send(fail.encode())
+        
+    #server_socket.shutdown(socket.SHUT_RD)    
+    server_socket.close()
+    server_socket = None
+    print("something recieved")
